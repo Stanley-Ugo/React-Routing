1e5760eb746ebb12d42695c58b1995e83ba26f26
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-05 00:56:10 +0100

    importing BrowserRouter & Route from react-router-dom - Index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Home from './components/home';
+import Posts from './components/post';
+import Profile from './components/profile';
 
 
 const App = () => {
