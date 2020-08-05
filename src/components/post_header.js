6a7b691d98b8c05ed4e06968dd3645f18344be9c
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-06 00:04:19 +0100

    Adding a PostHeader component and using withRouter

---
+++ src/components/post_header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { withRouter } from 'react-router';
+
+const PostsHeader = () => {
+    return (
+        <div>
+            Posts Header
+        </div>
+    )
+}
+
+export default withRouter(PostsHeader);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, NavLink, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, NavLink, Switch } from "react-router-dom";
 
 import Home from "./components/home";
 import Posts from "./components/post";
 import Profile from "./components/profile";
 import PostItem from "./components/post_item";
+import NotFound from './components/404';
 
 const App = () => {
   return (
@@ -47,6 +48,7 @@ const App = () => {
         <Route path="/posts" component={Posts} />
         <Route path="/profile" component={Profile} />
         <Route path="/" component={Home} />
+        <Route path={NotFound}/>
       </Switch>
     </BrowserRouter>
   );
