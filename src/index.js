c273752d6822e3cd1aabf1b246dc93d2262ad7cc
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-05 23:26:33 +0100

    adding Switch to Routes

--- src/index.js
+++ src/index.js
@@ -1,34 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link, HashRouter, MemoryRouter, NavLink } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, NavLink, Switch } from "react-router-dom";
 
-import Home from './components/home';
-import Posts from './components/post';
-import Profile from './components/profile';
-import PostItem from './components/post_item';
-
+import Home from "./components/home";
+import Posts from "./components/post";
+import Profile from "./components/profile";
+import PostItem from "./components/post_item";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <header>
-      <NavLink to="/" exact activeStyle={{ color: 'red'}} activeClassName="selected">Home</NavLink><br/>
-      <NavLink to="/posts" exact activeStyle={{ color: 'red'}} activeClassName="selected">Posts</NavLink><br/>
-      <NavLink to="/profile" exact activeStyle={{ color: 'red'}} activeClassName="selected">Profile</NavLink><br/>
-      <br/><br/>
-    </header>
+      <header>
+        <NavLink
+          to="/"
+          exact
+          activeStyle={{ color: "red" }}
+          activeClassName="selected"
+        >
+          Home
+        </NavLink>
+        <br />
+        <NavLink
+          to="/posts"
+          exact
+          activeStyle={{ color: "red" }}
+          activeClassName="selected"
+        >
+          Posts
+        </NavLink>
+        <br />
+        <NavLink
+          to="/profile"
+          exact
+          activeStyle={{ color: "red" }}
+          activeClassName="selected"
+        >
+          Profile
+        </NavLink>
+        <br />
+        <br />
+        <br />
+      </header>
 
-    <Route path="/" exact component={Home}/>
-    <Route path="/posts" exact component={Posts}/>
-    <Route path="/posts/:id" component={PostItem}/>
-    <Route path="/profile" exact component={Profile}/>
-  </BrowserRouter>
-  )
-}
+      <Switch>
+        <Route path="/posts/:id" component={PostItem} />
+        <Route path="/posts" component={Posts} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
