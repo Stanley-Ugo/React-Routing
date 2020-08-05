0eab6ae87a54cccd53c3f4f70429d0da346b3600
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-05 01:27:45 +0100

    importing Link from react-router & linking components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 import Home from './components/home';
 import Posts from './components/post';
@@ -11,7 +11,10 @@ const App = () => {
   return (
     <BrowserRouter>
     <header>
-      header
+      <Link to="/">Home</Link><br/>
+      <Link to="/posts">Posts</Link><br/>
+      <Link to="/profile">Profile</Link><br/>
+      <br/><br/>
     </header>
 
     <Route path="/" exact component={Home}/>
