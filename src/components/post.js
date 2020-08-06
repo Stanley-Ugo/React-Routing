9dbf913f54b674385b91ffa773740681382eac76
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-06 14:17:32 +0100

    returnig an Array of items - Adjacent Elements

--- src/components/post.js
+++ src/components/post.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Posts = () => {
-    return (
-        <div>
-            Posts
-        </div>
-    )
+    
+
+    const ids = [
+        {id: 1, name: 'Post 1'},
+        {id: 2, name: 'Post 2'},
+        {id: 3, name: 'Post 3'}
+    ]
+
+    // return ids.map(item =>(
+    //     <div key={item.id}>
+    //         <Link to={item.id}>{item.name}</Link>
+    //     </div>
+    // ))
+
+    return [
+        <div key='1'>Hello</div>,
+        <div key='2'>i am a</div>,
+        <div key='3'>React App</div>
+    ]
 }
 
 export default Posts;
